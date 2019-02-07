fe364f021304ab59372fad0aeec635fa32b767ae
M. Cirino <mecirino@gmail.com>
2019-02-07 11:05:17 -0800

    Update sgemm.cu

--- sgemm.cu
+++ sgemm.cu
@@ -170,7 +170,7 @@ int main(int argc, char ** argv){
   cout << " matrix (16): " 
 #endif
 
-<< size << ", ops: " << (size*size*size)*2 << endl << " average time: " << time << " s "<< endl;
+<< size << ", ops: " << endl << " average time: " << time << " s "<< endl;
   // GFLOPS: (m*n*k*2/time)/1e9
   }
 
