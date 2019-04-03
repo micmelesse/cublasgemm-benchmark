a6b3d55553f2beb335bdbe3b7854ecd1af566d6e
Cirino, Mary <mary.cirino@amd.com>
2019-04-03 12:27:56 -0700

    repeats set to 100

--- hgemm.cu
+++ hgemm.cu
@@ -62,7 +62,7 @@ int main(int argc, char ** argv){
 
   int min_m_k_n = 64; // min matrix size
   int max_m_k_n = 7744; // max matrix size
-  int repeats = 1;
+  int repeats = 100;
   int verbose = 1;
 
 #ifndef FP16MM
