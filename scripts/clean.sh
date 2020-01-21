d5d8409920b7e8ca245bd607cd9b5499d676ab29
Michael Melesse <mmelesse@ixt-rack-61.amd.com>
2020-01-21 14:42:13 -0800

    remove unnecesary scripts

--- scripts/clean.sh
+++
@@ -1,3 +0,0 @@
-rm -rf matrixMulCUBLAS/bin
-cd matrixMulCUBLAS/0_Simple/matrixMulCUBLAS
-make clean
--- scripts/download_sdk_samples.sh
+++
@@ -1,5 +0,0 @@
-apt update
-apt -y install git
-rm -rf nvidia_sdk_samples
-git clone https://github.com/pathscale/nvidia_sdk_samples
-cd nvidia_sdk_samples
